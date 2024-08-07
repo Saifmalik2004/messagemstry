3b7f6a7d93e614b7854c4ec3bafbcd35ba37e89b
saifmalik2004 <saifmalik7827@gmail.com>
2024-08-07 15:09:24 +0530

    add sigup page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,22 @@
-import React from 'react'
-
+"use client"
+ 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useState } from "react"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import {useDebounceValue} from'usehooks-ts'
+import { useToast } from "@/components/ui/use-toast"
+import { useRouter } from "next/navigation"
 function page() {
+
+  const [username,setUsername] = useState('')
+  const [usernameMessage, setUsernameMessage] = useState('')
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false)
+  const [iSubmitting, setSubmitting] = useState(false)
+
+  const debouncedUsername=useDebounceValue(username,300)
+   const {toast}= useToast()
+   const router = useRouter()
   return (
     <div>page</div>
   )
